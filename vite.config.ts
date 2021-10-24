5803b8e0b96b584e450d6d1596a8efc2cd978aa6
vinibispo <vini.bispo015@gmail.com>
2021-10-24 08:05:57 -0300

    chore(ts-paths): updating ts-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()]
+  plugins: [react(), eslintPlugin(), tsconfigPaths()]
 })
