ce2f206e7127d9478e489b00962d170d503a561b
vinibispo <vini.bispo015@gmail.com>
2021-10-24 08:24:06 -0300

    feat(pwa): add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths(), VitePWA({
+    strategies: 'injectManifest',
+    manifest: {
+
+      short_name: 'Markee App',
+      name: 'Markdown Editor',
+      icons: [
+        {
+          src: 'favicon.png',
+          sizes: '64x64 32x32 24x24 16x16',
+          type: 'image/png'
+        },
+        {
+          src: 'logo192.png',
+          type: 'image/png',
+          sizes: '192x192'
+        },
+        {
+          src: 'logo512.png',
+          type: 'image/png',
+          sizes: '512x512'
+        }
+      ],
+      start_url: '.',
+      display: 'standalone',
+      theme_color: '#1E293B',
+      background_color: '#ffffff'
+    }
+  })]
 })
