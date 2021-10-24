d56e0e0b6d59cf77dc273b3dac20442199b48852
vinibispo <vini.bispo015@gmail.com>
2021-10-24 09:39:01 -0300

    feat(styled): add styled-components

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
+import styled from 'styled-components'
 
 export function App () {
   return (
-    <div>App</div>
+    <Title>App</Title>
   )
 }
+
+const Title = styled.h1`
+color: ${props => props.theme.colors.primary};
+`
--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,18 @@
 import { App } from 'app'
+import { theme } from 'resources/theme'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 
 export function Root () {
   return (
-  <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+  * { box-sizing: border-box; }
+  html { font-size: 62.5%; }
+  body { font-family: 'DM Sans', sans-serif; }
+`
