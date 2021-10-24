4f0289786f6c1a6a79e3de120e38520e59586ec1
vinibispo <vini.bispo015@gmail.com>
2021-10-24 11:07:55 -0300

    feat(aside): add aside component

---
+++ src/aside/aside.tsx
@@ -0,0 +1,20 @@
+// import { File } from 'resources/files/types'
+// const files: File[] = [
+//   {
+//     id: '0',
+//     name: 'README.md',
+//     content: 'Conteúdo do README',
+//     active: false,
+//     status: 'saved'
+//   },
+//   {
+//     id: '1',
+//     name: 'CONTRIBUTING.md',
+//     content: 'Conteúdo do Contributing',
+//     active: true,
+//     status: 'editing'
+//   }
+// ]
+export function Aside () {
+  return (<aside />)
+}
