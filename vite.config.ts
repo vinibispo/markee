369ff64ee50f18768d5d167c7806eab613335c4e
vinibispo <vini.bispo015@gmail.com>
2021-11-15 21:01:03 -0300

    feat(svgr): add support for ReactComponent

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({
+  plugins: [react(), tsconfigPaths(), svgr(), VitePWA({
     strategies: 'injectManifest',
     manifest: {
 
