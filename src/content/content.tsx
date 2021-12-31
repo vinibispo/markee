329de8b07b119e933a2b6ef63080046250e487db
vinibispo <vini.bispo015@gmail.com>
2021-12-31 11:55:16 -0300

    feat(markdown): render markdown with security

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,16 +1,34 @@
+import { ChangeEvent, useState } from 'react'
 import * as S from './content-styles'
+import { marked } from 'marked'
+import 'highlight.js/styles/github.css'
+import DOMPurify from 'dompurify'
+import('highlight.js').then(hljs => {
+  const highlight = hljs.default
+
+  marked.setOptions({
+    highlight: (code, language) => {
+      if (language && highlight.getLanguage(language)) {
+        return highlight.highlight(code, { language }).value
+      }
+      return highlight.highlightAuto(code).value
+    }
+  })
+})
+
 export function Content () {
+  const [content, setContent] = useState('')
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value)
+  }
   return (
     <S.ContentWrapper>
       <S.Header>
         <S.Input defaultValue='Sem título' />
       </S.Header>
       <S.ContentSection>
-        <S.Textarea placeholder='Digite aqui seu markdown' />
-        <S.Article>
-          <h1>Bootcamp Brainn Co.</h1>
-          <p>Lorem ipsum dolor sit amet simet</p>
-        </S.Article>
+        <S.Textarea placeholder='Digite aqui seu markdown' value={content} onChange={handleChange} />
+        <S.Article dangerouslySetInnerHTML={{ __html: marked(DOMPurify.sanitize(content)) }} />
       </S.ContentSection>
     </S.ContentWrapper>
   )
